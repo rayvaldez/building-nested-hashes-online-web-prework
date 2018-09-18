95a520641309dd3c84c5c86ea1e66f3a48888768
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-18 16:22:47 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,14 +2,14 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       patriarch: {},
-      matriarch: {}, 
-      hero: {}, 
+      matriarch: {},
+      hero: {},
       hero_friends: {}
       },
     :capulet => {
-      patriarch: {}, 
-      matriarch: {}, 
-      heroine: {}, 
+      patriarch: {},
+      matriarch: {},
+      heroine: {},
       heroine_friends: {}
       }}
 
