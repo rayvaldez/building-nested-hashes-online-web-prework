074e1cfcb03dde7884fe39a6616931886bc0a535
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-18 16:37:39 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -24,7 +24,7 @@ def fourth_challenge
         {
         name: "Steven",
         age: "30",
-        attitude: "confused"}
+        attitude: "confused"},
         {
         name: "Nurse",
         age: "44",
