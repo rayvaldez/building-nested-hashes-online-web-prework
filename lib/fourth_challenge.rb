301df8cbca067b25f35cf51dad15a05fd564cae6
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-18 16:36:28 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -15,7 +15,7 @@ def fourth_challenge
           attitude: "hot-headed"
         }
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -28,12 +28,12 @@ def fourth_challenge
         {
         name: "Nurse",
         age: "44",
-        attitude: "worried"  
+        attitude: "worried"
         }
       ]
    }
 }
 
-  
+
 
 end
